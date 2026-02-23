7ee0c986bfd1eab232e7f68a0e16048fd1721dec
dolinskih <90028827+dolinskih@users.noreply.github.com>
2026-02-23 19:29:44 +0100

    Added panel toggle animation

--- floxt/app/panel/layout.tsx
+++ floxt/app/panel/layout.tsx
@@ -15,35 +15,38 @@ export default function PanelLayout() {
     const [isOpen, setIsOpen] = useState(true);
 
     return (
-        <section className={`p-3 hover:shadow-lg dark:shadow-white/15 ${isOpen ? 'pr-5' : ''}`}>
+        <section className={`p-3 hover:shadow-lg dark:shadow-white/15 transition-all duration-150 ease-in-out w-fit ${isOpen ? 'pr-5' : ''}`}>
             {isOpen && (
-                <>
+                <div
+                    className={`overflow-hidden transition-all duration-150 ease-in-out flex flex-col ${isOpen ? 'max-h-[500px] max-w-[300px] opacity-100' : 'max-h-0 max-w-0 opacity-0'
+                        }`}
+                >
                     <button className={"flex items-center active:scale-95 active:opacity-75 hover:opacity-75 transition-opacity delay-100 ease-in-out cursor-pointer"}>
                         <Plus color="white" size={28} className="mt-2 mb-2" />
-                        <p className="m-2 mr-5">New note</p>
+                        <p className="m-2 mr-5 whitespace-nowrap">New note</p>
                     </button>
                     <button className={"flex items-center active:scale-95 active:opacity-75 hover:opacity-75 transition-opacity delay-100 ease-in-out cursor-pointer"}>
                         <FolderOpen color="white" size={28} className="mt-2 mb-2" />
-                        <p className="m-2 mr-5">Open file</p>
+                        <p className="m-2 mr-5 whitespace-nowrap">Open file</p>
                     </button>
                     <button className={"flex items-center active:scale-95 active:opacity-75 hover:opacity-75 transition-opacity delay-100 ease-in-out cursor-pointer"}>
                         <Terminal color="white" size={28} className="mt-2 mb-2" />
-                        <p className="m-2 mr-5">Commands</p>
+                        <p className="m-2 mr-5 whitespace-nowrap">Commands</p>
                     </button>
                     <button className={"flex items-center active:scale-95 active:opacity-75 hover:opacity-75 transition-opacity delay-100 ease-in-out cursor-pointer"}>
                         <Cog color="white" size={28} className="mt-2 mb-2" />
-                        <p className="m-2 mr-5">Settings</p>
+                        <p className="m-2 mr-5 whitespace-nowrap">Settings</p>
                     </button>
-                </>
+                </div>
             )}
 
-            <button 
+            <button
                 onClick={() => setIsOpen(!isOpen)}
-                className={`flex items-center active:scale-95 active:opacity-75 hover:opacity-75 transition-opacity delay-100 ease-in-out cursor-pointer ${isOpen ? 'mt-5' : ''}`}>
+                className={`flex items-center active:scale-95 active:opacity-75 hover:opacity-75 transition-all duration-150 ease-in-out cursor-pointer ${isOpen ? 'mt-5' : ''}`}>
                 {isOpen ? (
                     <ChevronUp color="white" size={28} className="mt-2 mb-2" />
                 ) : (
-                    <ChevronDown color="white" size={28} className="mt-2 mb-2"/>
+                    <ChevronDown color="white" size={28} className="mt-2 mb-2" />
                 )}
             </button>
         </section>
